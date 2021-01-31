72a3fbcfa1a243fee0fab14946867b3a22369309
Bola_sw25@student.itstep.kz <Bola_sw25@student.itstep.kz>
2021-01-31 22:11:26 +0600

    Добавьте файлы проекта.

---
+++ PracticalWork6/ArmoredVehicles.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticalWork7
+{
+    class ArmoredVehicles
+    {
+        public string name;
+        public int caliber = 125;   
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Наименование: {name}  Калибр: {caliber}");
+        }
+    }
+    class Tank
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ PracticalWork6/TestTank.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticalWork7
+{
+    class TestTank : Tank
+    {
+
+        public override double GetTopCaliber()
+        {
+            return 125;
+        }
+
+        static void Main()
+        {
+
+            TestTank characterstic = new TestTank();
+
+            characterstic.StartWeight(54);
+            characterstic.AddDiesel(500);
+            characterstic.Armor(90, 150, 135);
+            double penetration = characterstic.GetTopPenetration(140);
+            Console.WriteLine("My top penetration is {138}", penetration);
+        }
+
+        private void Armor(int v1, int v2, int v3)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddDiesel(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void StartWeight(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private double GetTopPenetration(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
